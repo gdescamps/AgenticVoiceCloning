874fcb689d0ba1eff042baf94ef846e85ce0cd52
Gregory DESCAMPS <descamps.gregory@gmail.com>
2025-11-16 04:10:22 +0100

    Reorder setup script to initialize git submodules before removing virtual environment

--- 1_setup.sh
+++ 1_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rm -rf venv
+git submodule update --init --recursive
 
+rm -rf venv
 python3.12 -m venv venv
 source venv/bin/activate
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu130
